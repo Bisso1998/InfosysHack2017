d6e1b411e1f63c0044f9f1aacb4b4939bd036c64
rowhitswami <rowhitswami1@gmail.com>
2017-09-24 07:55:51 +0530

    fixed typo

--- assets/firebase.js
+++ assets/firebase.js
@@ -32,6 +32,20 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 });
 
+
+
+// auth2 is initialized with gapi.auth2.init() and a user is signed in.
+
+if (auth2.isSignedIn.get()) {
+  var profile = auth2.currentUser.get().getBasicProfile();
+  console.log('ID: ' + profile.getId());
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+}
+
 //   name = user.displayName;
 //   email = user.email;
 //   photoUrl = user.photoURL;
